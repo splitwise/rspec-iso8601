603160652fb8e38bc54421f256906b1c1c7140bc
Joe Stein <joe@splitwise.com>
2022-07-01 15:39:25 -0400

    Add negative matcher tests

--- spec/rspec/matchers_spec.rb
+++ spec/rspec/matchers_spec.rb
@@ -24,4 +24,16 @@ RSpec.describe "be_an_iso8601_string" do
 
     specify { expect(string).to be_an_iso8601_string }
   end
+
+  context "with an invalid string" do
+    let(:string) { "ceci n'est pas une ISO8601 string" }
+
+    specify { expect(string).not_to be_an_iso8601_string }
+  end
+
+  context "with invalid types" do
+    specify { expect(:abc).not_to be_an_iso8601_string }
+    specify { expect(123).not_to be_an_iso8601_string }
+    specify { expect({ hi: :there }).not_to be_an_iso8601_string }
+  end
 end
