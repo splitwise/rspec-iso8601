ee1e74df02c6d373d35b0954bdce9afb1505c292
Joe Stein <joe@splitwise.com>
2022-07-01 15:48:24 -0400

    Add a microsecond precision matcher argument

--- lib/rspec/iso8601.rb
+++ lib/rspec/iso8601.rb
@@ -2,15 +2,16 @@
 
 require "rspec/expectations"
 
-RSpec::Matchers.define :be_an_iso8601_string do
+RSpec::Matchers.define :be_an_iso8601_string do |precision: nil|
   match do |actual|
     return false unless actual.is_a?(String)
 
-    actual.match?(/^\d{4}-\d{2}-\d{2}T(?:\d{2}:){2}\d{2}(?:\.(?<usec>\d+))?(?:Z|[\-+]\d{2}:\d{2})$/)
+    matches = actual.match(/^\d{4}-\d{2}-\d{2}T(?:\d{2}:){2}\d{2}(?:\.(?<usec>\d+))?(?:Z|[\-+]\d{2}:\d{2})$/)
+    matches && (precision.nil? || (matches[:usec]&.length || 0) == precision)
   end
 
   description do
-    "be an ISO8601 string"
+    "be an ISO8601 string#{" with precision #{precision}" unless precision.nil?}"
   end
 end
 
