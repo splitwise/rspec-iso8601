c647e6e559f6b7bdf371d03bb08a00658986ef16
Jess Hottenstein <jess@splitwise.com>
2022-07-01 15:14:37 -0400

    Add ISO8601 string matcher

--- lib/rspec/iso8601.rb
+++ lib/rspec/iso8601.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "iso8601/version"
+require "rspec/expectations"
 
-module Rspec
-  module ISO8601
-    class Error < StandardError; end
-    # Your code goes here...
+RSpec::Matchers.define :be_an_iso8601_string do
+  match do |actual|
+    return false unless actual.is_a?(String)
+
+    actual.match?(/^\d{4}-\d{2}-\d{2}T(?:\d{2}:){2}\d{2}(?:\.(?<usec>\d+))?(?:Z|[\-+]\d{2}:\d{2})$/)
+  end
+
+  description do
+    "be an ISO8601 string"
   end
 end
+
+RSpec::Matchers.alias_matcher :be_an_iso8601_string, :an_iso8601_string
