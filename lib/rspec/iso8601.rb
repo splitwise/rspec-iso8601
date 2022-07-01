8560174078499f562082fc79e2aa25875ad8f736
Joe Stein <joe@splitwise.com>
2022-07-01 16:08:17 -0400

    Move regexp to constant in Rspec::ISO8601

--- lib/rspec/iso8601.rb
+++ lib/rspec/iso8601.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rspec/expectations"
+require_relative "iso8601/regexp"
 
 RSpec::Matchers.define :be_an_iso8601_string do |precision: nil|
   match do |actual|
@@ -8,7 +9,7 @@ RSpec::Matchers.define :be_an_iso8601_string do |precision: nil|
 
     return false unless actual.is_a?(String)
 
-    matches = actual.match(/^\d{4}-\d{2}-\d{2}T(?:\d{2}:){2}\d{2}(?:\.(?<usec>\d+))?(?:Z|[\-+]\d{2}:\d{2})$/)
+    matches = actual.match(Rspec::ISO8601::REGEXP)
 
     precision ||= @precision
     matches && (precision.nil? || (matches[:usec]&.length || 0) == precision)
